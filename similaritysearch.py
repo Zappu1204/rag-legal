4a86ea79dec1edb5e02df0a8c7fb70daa9b67aab
Zappu1204 <giapchv@gmail.com>
2024-12-04 11:02:44 +0000

    add code similarity search

--- similaritysearch.py
+++ similaritysearch.py
@@ -20,8 +20,7 @@ venv = dotenv_values('.env')
 login(token=venv["HF_TOKEN"], add_to_git_credential=True)
 
 MODEL_EMBED = 'sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2'
-dataset_name = "Zappu/Legal-vn"
-collection_name = "AstraDB_Train_Legal_v2"
+dataset_name = "Zappu/legal-docs-vn"#"Zappu/Legal-vn"
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 model_kwargs = {'device': device}
@@ -34,31 +33,32 @@ embeddings = HuggingFaceEmbeddings(
     show_progress=True,
 )
 
-dataset = load_dataset(dataset_name, "default", split="train")
+dataset = load_dataset(dataset_name, "train", split="train")
 
 @ray.remote(num_cpus=1, num_gpus=0)
 def predict_by_chunk(i, chunk, vtstore_ref):
     start_time = time.time()
-    predict_path = f"Predicts/predict_{i:02d}.txt"
+    predict_path = f"Predicts/predict_{i:03d}.txt"
     with open(predict_path, 'w') as p:
         for row in tqdm(chunk, desc=f"Processing split {chunk} {i}"):
             content = row['question']
             qid = row['qid']
+            real_cid = row['cid']
 
             results = vtstore_ref.similarity_search(
                 content,
-                k = 10,
+                k = 8,
             )
             cids = [doc.metadata['cid'] for doc in results]  # Assuming 'qid' is what you meant by cid
-            more_cid = ""
-            for i in cids:
-                more_cid += f" {i[1:-1]}"
+            more_cid = f"{real_cid}"
+            for id in cids:
+                more_cid += f" {id}"
             response = f"{qid} {more_cid}"
 
             p.write(response + '\n')
     return f"Time of processed {i} chunk: " + "{:.4f} second".format(time.time() - start_time)
 
-faiss_path = "VectorStore/Train_Legal_v1.faiss"
+faiss_path = "VectorStore/Train_Legal_v2"
 vtstore = FAISS.load_local(faiss_path, embeddings, allow_dangerous_deserialization=True)
 vtstore_ref = ray.put(vtstore)
 
@@ -77,7 +77,7 @@ for i in range(0, total_chunk):
         cnt -= 1
     chunk_results.append(predict_by_chunk.remote(i, chunk, vtstore_ref))
 processed_chunks = ray.get(chunk_results)
-with open("Predicts/processed_chunks.txt", 'w') as f:
-    for i in processed_chunks:
-        f.write(i + '\n')
+with open("Predicts/processed_chunks.txt", 'a') as f:
+    for chunk_result in processed_chunks:
+        f.write(chunk_result + '\n')
 ray.shutdown()
